31ad7d7e33f899bc3949163b21cd0f34e5135103
seo-rio <scs9310@gmail.com>
2021-05-20 17:16:51 +0900

    plugin 사용법 익히기 커밋

--- src/app.js
+++ src/app.js
@@ -8,3 +8,7 @@ import nyancat from "./nyancat.jpg";
 document.addEventListener("DOMContentLoaded", () => {
   document.body.innerHTML = `<img src="${nyancat}" /> `;
 });
+
+console.log(process.env.NODE_ENV);
+console.log(TWO);
+console.log(api.domain);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require("path");
+// const MyWebpackPlugin = require("./my-webpack-plugin");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -19,7 +25,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        // use: ["style-loader", "css-loader"],
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ],
       },
       // {
       //   test: /\.(png|jpg)$/,
@@ -33,11 +45,46 @@ module.exports = {
         test: /\.(png|jpg)$/,
         loader: "url-loader",
         options: {
-          publicPath: "./dist/",
+          // publicPath: "./dist/",
           name: "[name].[ext]?[hash]",
           limit: 20000, // 20kb
         },
       },
     ],
   },
+  plugins: [
+    // new MyWebpackPlugin()
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    new webpack.DefinePlugin({
+      TWO: JSON.stringify("1+1"),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin({}),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          new MiniCssExtractPlugin({
+            filename: "[name].css",
+          }),
+        ]
+      : []),
+  ],
 };
