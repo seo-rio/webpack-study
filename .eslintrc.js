61d234521bc33ca31b685aa27d728a5a8430659f
seo-rio <scs9310@gmail.com>
2021-06-02 16:57:30 +0900

    eslint --init 사용하여 설정파일 구성

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
-    rules: {
-      "no-unexpected-multiline": "error",
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  }
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-var foo = bar
-(1 || 2).baz();
-
-var hello = 'world'
-[1, 2, 3].forEach(addNumber);
-
-let x = function() {}
-`hello`
-
-let x = function() {}
-x
-`hello`
-
-let x = foo
-/regex/g.test(bar)
+console.log();
