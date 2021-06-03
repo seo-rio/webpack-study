a6a6e33c62171ba05fbaefc01b5743e2fb9a4693
seo-rio <scs9310@gmail.com>
2021-06-03 17:48:51 +0900

    Prettier & ESLint 통합 설정 구성

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,15 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "eslint-config-prettier"],
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
     },
+    "plugins": [
+        "prettier"
+    ],
     "rules": {
-    }
+        "prettier/prettier": "error"
+    },
 };
--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.log();
+var foo = ""; // 사용하지 않은 변수.
+console.log(); // 중복 세미콜론 사용.
