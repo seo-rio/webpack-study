52f97b2708bf770ee0fa10413a620353717050d0
seo-rio <scs9310@gmail.com>
2021-06-08 17:50:11 +0900

    webpack-dev-server 기본 설정

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@
 
 // console.log(math.sum(1, 2));
 
-import "./app.css";
-import nyancat from "./nyancat.jpg";
+import './app.css';
+import nyancat from './nyancat.jpg';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   document.body.innerHTML = `<img src="${nyancat}" /> `;
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,16 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    // main: "./src/app.js",
-    main: './app.js',
+    main: './src/app.js',
+    // main: './app.js',
   },
   output: {
     path: path.resolve('./dist'), // node의 path 모듈을 가져와 절대경로를 삽입한다.
     filename: '[name].js', // 번들링된 파일명을 설정 [name]은 entry에서 설정한 key값인 main이 들어간다.
+  },
+  devServer: {
+    overlay: true,
+    stats: 'errors-only',
   },
   module: {
     // rules: [
